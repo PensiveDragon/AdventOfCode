e324bbba21950f76ef971d04511759d95e5b976d
PensiveDragon <matthew_bfg2k@hotmail.com>
2021-12-08 23:34:04 +0000

    DayOne method notes written

---
+++ src/main/java/DayOne.java
@@ -0,0 +1,10 @@
+public class DayOne {
+
+    public static void main(String[] args) {
+
+        //input list into array
+        //iterate through list
+        //compare item to previous item
+        //if larger, increment increase depth list
+    }
+}
