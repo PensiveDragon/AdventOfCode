73f5f446e7d8588381fbc699ac965bd7267b22f8
PensiveDragon <matthew_bfg2k@hotmail.com>
2021-12-12 00:12:05 +0000

    Part One of puzzle solved!

--- src/main/java/DayOne.java
+++ src/main/java/DayOne.java
@@ -13,41 +13,26 @@ public class DayOne {
 
     public static void main(String[] args) {
 
-        String testInput = "199\n200\n208\n210\n200\n207\n240\n269\n260\n263";
+
         String inputTwo = "";
         int depthLevel = Integer.MAX_VALUE;
         int incrementCounter = 0;
-
+        List<Integer> depths;
+        String testInput = "199\n200\n208\n210\n200\n207\n240\n269\n260\n263";
         String[] depthStrings = testInput.split("\n");
 
-        /*
-        BufferedReader reader;
-        try {
-            reader = new BufferedReader(new FileReader(
-                    "src/main/resources/day1_input.txt"));
-            inputTwo = reader.readLine();
-            while (inputTwo != null) {
-                System.out.println(inputTwo);
-                inputTwo = reader.readLine();
-            }
-            reader.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        System.out.println("InputTwo = " + inputTwo);
-*/
+        depths = Arrays.stream(depthStrings)
+                .map(s -> Integer.parseInt(s))
+                .collect(Collectors.toList());
 
         try {
             List<String> allLines = Files.readAllLines(Paths.get("src/main/resources/day1_input.txt"));
-            for (String line : allLines) {
-                System.out.println(line);
-            }
+            depthStrings = allLines.toArray(new String[0]);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        List<Integer> depths = Arrays.stream(depthStrings)
+        depths = Arrays.stream(depthStrings)
                 .map(s -> Integer.parseInt(s))
                 .collect(Collectors.toList());
 
