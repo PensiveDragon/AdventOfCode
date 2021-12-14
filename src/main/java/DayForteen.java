e90277f8f0532986f0627a8b903bfafed4d6e7dc
PensiveDragon <matthew_bfg2k@hotmail.com>
2021-12-14 23:49:29 +0000

    Plan for DayForteen Puzzle

---
+++ src/main/java/DayForteen.java
@@ -0,0 +1,14 @@
+public class DayForteen {
+
+    public static void main(String[] args) {
+        // Repeat 10 times
+        // > Take String existingString
+        // > Iterate two letters through the loop
+        // > Merge the two letters into a pair
+        // > Compare the pair against the list of options
+        // > If a match is found, find the new character
+        // > Add the letters to the String nextString
+        // Count the number of each letter in the final String
+        // Subtract the lowest number from the  highest number.
+    }
+}
