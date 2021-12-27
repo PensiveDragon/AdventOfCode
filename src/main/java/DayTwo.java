9c9a6873ce061806dc3aae1e1f90f8b9aba251d5
Matthew Sherman <matthew.sherman@masabi.com>
2021-12-27 23:53:11 +0000

    Basic setup off DayTwo puzzle solution and notes.

---
+++ src/main/java/DayTwo.java
@@ -0,0 +1,12 @@
+public class DayTwo {
+
+    public static void main (String[] args) {
+        System.out.println("moo");
+
+        // Write method to parse through file
+        // Two ints: distance and depth
+        // Read file, adding for distance and adding/removing for depth
+        // Multiply final distance by final depth
+    }
+
+}
