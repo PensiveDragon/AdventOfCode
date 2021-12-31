b02078c5c3421c4cdfdaf1964fe9bb5620923ab4
PensiveDragon <matthew_bfg2k@hotmail.com>
2021-12-31 00:33:11 +0000

    DayTwo solvePuzzle method implemented - Part One Complete!

--- src/main/java/DayTwo.java
+++ src/main/java/DayTwo.java
@@ -13,9 +13,11 @@ public class DayTwo {
 
         String[] input = parseInput();
 
-        String[] testInput = {"forward 5", "down 5", "forward 8", "up 3", "down 8", "forward 2"};
+        //String[] testInput = {"forward 5", "down 5", "forward 8", "up 3", "down 8", "forward 2"};
 
-        readInput(testInput);
+        readInput(input);
+
+        System.out.println("The answer is: " + solvePuzzle(depth, distance));
 
 
 
@@ -23,6 +25,11 @@ public class DayTwo {
         // Two ints: distance and depth
         // Read file, adding for distance and adding/removing for depth
         // Multiply final distance by final depth
+    }
+
+    public static int solvePuzzle(int depth, int distance) {
+        int result = depth * distance;
+        return result;
     }
 
     public static void readInput(String[] input) {
