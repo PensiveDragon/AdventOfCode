627fdf44d5d9e76629b0ab02e5563e0d320ca0ac
PensiveDragon <matthew_bfg2k@hotmail.com>
2021-12-31 00:30:10 +0000

    DayTwo readInput method implemented

--- src/main/java/DayTwo.java
+++ src/main/java/DayTwo.java
@@ -5,15 +5,40 @@ import java.util.List;
 
 public class DayTwo {
 
+    static int distance = 0;
+    static int depth = 0;
+
     public static void main (String[] args) {
         System.out.println("moo");
 
         String[] input = parseInput();
 
+        String[] testInput = {"forward 5", "down 5", "forward 8", "up 3", "down 8", "forward 2"};
+
+        readInput(testInput);
+
+
+
         // Write method to parse through file
         // Two ints: distance and depth
         // Read file, adding for distance and adding/removing for depth
         // Multiply final distance by final depth
+    }
+
+    public static void readInput(String[] input) {
+
+        for (String line : input){
+            if (line.contains("up")) {
+                depth -= Integer.parseInt(String.valueOf(line.charAt(line.length()-1)));
+                System.out.println("Current depth = " + depth);
+            } else if (line.contains("down")) {
+                depth += Integer.parseInt(String.valueOf(line.charAt(line.length()-1)));
+                System.out.println("Current depth = " + depth);
+            } else if (line.contains("forward")) {
+                distance += Integer.parseInt(String.valueOf(line.charAt(line.length()-1)));
+                System.out.println("Current distance = " + distance);
+            }
+        }
     }
 
     public static String[] parseInput() {
