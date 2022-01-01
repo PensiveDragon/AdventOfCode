68d4aaa0852bbd7e2c2f6137a3c90f7f10cee1b9
PensiveDragon <matthew_bfg2k@hotmail.com>
2022-01-01 23:57:30 +0000

    DayThree created. Input Text file created. Plan to solve written.

---
+++ src/main/java/DayThree.java
@@ -0,0 +1,12 @@
+public class DayThree {
+
+    public static void main (String args[]) {
+        System.out.println("moo");
+
+        // Parse file into string[].
+        // Go through string[] line by line, char by char adding chars to a corresponding bit sum array.
+        // Sum of each bit array tells you how many 1s were present, if >500, it is a 1, <500 it is a 0.
+        // Create two values from number and inverse number.
+        // Multiply together for answer.
+    }
+}
