ff40ca72b2fd1398cb677f473c6cfd32449e75cb
PensiveDragon <matthew_bfg2k@hotmail.com>
2022-01-24 23:59:27 +0000

    DayFour puzzle method

---
+++ src/main/java/DayFour.java
@@ -0,0 +1,11 @@
+public class DayFour {
+
+    // Puzzle input: n*(5x5) boards = 3d array?
+    // Puzzle input: Array(m) of drawn numbers (0-99)
+
+    // Parse input data into both data structures
+    // Cycle through drawn numbers. For each number:
+    // - Check through each board, marking the found number in each (updating the found number in each to a *)?
+    // - Check whether any board has won yet
+    // On win, sum all unmarked numbers on board, multiply by last number drawn.
+}
