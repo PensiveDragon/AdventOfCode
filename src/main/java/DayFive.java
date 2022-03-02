744afc97ff328301492deafd069e6d60a3a7058f
PensiveDragon <matthew_bfg2k@hotmail.com>
2022-03-02 23:59:28 +0000

    DayFive Basic setup and notes for solving the problem

---
+++ src/main/java/DayFive.java
@@ -0,0 +1,14 @@
+public class DayFive {
+
+    // Create 10x10 Grid
+    // Parse List input
+    // For each line of input
+    // -> Use coords to update grid with lines
+    // -> Each line adds 1 to the coords listed
+    // Count number of coords >1
+
+    public static void main(String[] args) {
+        System.out.println("Moo");
+    }
+
+}
