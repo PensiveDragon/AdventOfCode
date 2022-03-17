b5cddfdafa3afd63828f58df0e787e60c8076627
PensiveDragon <matthew_bfg2k@hotmail.com>
2022-03-17 23:53:56 +0000

    DaySix Starting point: basic frame and notes written.

---
+++ src/main/java/DaySix.java
@@ -0,0 +1,12 @@
+public class DaySix {
+
+    // Parse input
+    // Create a list of numbers populated by different quantities of fish
+    // Loop for x
+    // > Method iterating fish down ranks of values
+    // > Fish at value 0 reset to 6 and create new fish at 8
+
+    public static void main (String args[]) {
+        System.out.println("Moo");
+    }
+}
