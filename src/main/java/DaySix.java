20daddb1b415385ec632ba6030036410083a20ca
PensiveDragon <matthew_bfg2k@hotmail.com>
2022-03-20 23:33:46 +0000

    DaySix Switched to use custom Class lanternFishSchool.

--- src/main/java/DaySix.java
+++ src/main/java/DaySix.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -17,27 +18,48 @@ public class DaySix {
 
     public static void main(String args[]) {
         System.out.println("Moo");
+
+        String inputString = "src/main/resources/day6_input.txt";
+
+        parseInput(inputString);
     }
 
-    public static LinkedHashMap<String, Integer> setUpEvolutionTracker() {
-        LinkedHashMap<String, Integer> evolutionTracker = new LinkedHashMap<String, Integer>();
-        evolutionTracker.put("internalKeyZero", 0);
-        evolutionTracker.put("internalKeyOne", 0);
-        evolutionTracker.put("internalKeyTwo", 0);
-        evolutionTracker.put("internalKeyThree", 0);
-        evolutionTracker.put("internalKeyFour", 0);
-        evolutionTracker.put("internalKeyFive", 0);
-        evolutionTracker.put("internalKeySix", 0);
-        evolutionTracker.put("internalKeySeven", 0);
-        evolutionTracker.put("internalKeyEight", 0);
+    public static ArrayList<lanternFishSchool> setUpEvolutionTracker() {
+        ArrayList<lanternFishSchool> evolutionTracker = new ArrayList<lanternFishSchool>(8);
+        evolutionTracker.add(new lanternFishSchool("zero", 0));
+        evolutionTracker.add(new lanternFishSchool("one", 1));
+        evolutionTracker.add(new lanternFishSchool("two", 2));
+        evolutionTracker.add(new lanternFishSchool("three", 3));
+        evolutionTracker.add(new lanternFishSchool("four", 4));
+        evolutionTracker.add(new lanternFishSchool("five", 5));
+        evolutionTracker.add(new lanternFishSchool("six", 6));
+        evolutionTracker.add(new lanternFishSchool("seven", 7));
+        evolutionTracker.add(new lanternFishSchool("eight", 8));
 
         return evolutionTracker;
     }
 
-    public static LinkedHashMap<String, Integer> parseInput(String inputLocation) {
-        LinkedHashMap<String, Integer> evolutionTracker = setUpEvolutionTracker();
+    public static ArrayList<lanternFishSchool> parseInput(String inputLocation) {
+        ArrayList<lanternFishSchool> lanternFishSchools = setUpEvolutionTracker();
+
+        ArrayList<String> drawnNumbersList = new ArrayList<>();
+
+        try {
+            List<String> input = Files.readAllLines(Paths.get(inputLocation));
+
+            for (int i = 0; i < input.size(); i++) {
+                drawnNumbersList = new ArrayList<>(Arrays.asList(input.get(i).split(",")));
+            }
+
+            for (String num : drawnNumbersList) {
+                System.out.println(num);
+
+            }
 
 
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         return evolutionTracker;
     }
@@ -70,5 +92,15 @@ public class DaySix {
         }
 
         return null;
+    }
+}
+
+class lanternFishSchool {
+    String bracket_name = "";
+    int bracket_population = 0;
+
+    public lanternFishSchool(String bracket_name, int bracket_population) {
+        this.bracket_name = bracket_name;
+        this.bracket_population = bracket_population;
     }
 }
