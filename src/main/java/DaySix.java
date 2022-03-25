202ebf6eb1edce47e84cbd2d3a5ccc827ab72c6d
PensiveDragon <matthew_bfg2k@hotmail.com>
2022-03-25 00:04:55 +0000

    DaySix Part 1 Complete!!  Added notes for Part 2...

--- src/main/java/DaySix.java
+++ src/main/java/DaySix.java
@@ -15,9 +15,10 @@ public class DaySix {
     // Loop for x
     // > Method iterating fish down ranks of values
     // > Fish at value 0 reset to 6 and create new fish at 8
+    // Change counting numerics to bigger types to accommodate higher values.
 
     static int currentDay = 0;
-    static int numberOfDays = 18;
+    static int numberOfDays = 80;
 
     public static void main(String args[]) {
         System.out.println("Moo");
