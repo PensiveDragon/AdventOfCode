5f9f6dc4f49580cfe27e83a64107c62502aa328e
PensiveDragon <matthew_bfg2k@hotmail.com>
2022-03-28 23:57:42 +0100

    DayEight Basic set up and starting notes.

---
+++ src/main/java/DayEight.java
@@ -0,0 +1,11 @@
+public class DayEight {
+
+    // Reverse engineer input values to determine which numbers they correspond to
+    // Find 1, 4, 7, 8 as they use unique numbers of inputs.
+
+
+    public static void main (String args[]) {
+        System.out.println("Moo");
+    }
+
+}
