e6ecc065b06368436ed6efa5c35c21a50b331dba
PensiveDragon <matthew_bfg2k@hotmail.com>
2022-04-06 00:15:31 +0100

    DayEight finished countUniqueValues method

--- src/main/java/DayEight.java
+++ src/main/java/DayEight.java
@@ -15,6 +15,8 @@ public class DayEight {
     static String testInputPath = "src/main/resources/day8_test_input.txt";
     static String inputPath = "src/main/resources/day8_input.txt";
 
+    static int numberOfUniqueValues = 0;
+
     public static void main (String args[]) {
         System.out.println("Moo");
         ArrayList<String> input = parseInput(testInputPath);
@@ -23,11 +25,26 @@ public class DayEight {
     }
 
     public static void countUniqueValues (ArrayList<String> lineOutput) {
+
+
         for (String string : lineOutput) {
             if (string.length() == 2) {
                 System.out.println("This is a 1!");
+                numberOfUniqueValues++;
+            } else if (string.length() == 4) {
+                System.out.println("This is a 4!");
+                numberOfUniqueValues++;
+            } else if (string.length() == 3) {
+                System.out.println("This is a 7!");
+                numberOfUniqueValues++;
+            } else if (string.length() == 7) {
+                System.out.println("This is a 8!");
+                numberOfUniqueValues++;
+            } else {
+                System.out.println("This is another number");
             }
         }
+        System.out.println("There are this many unique value numbers: " + numberOfUniqueValues);
     }
 
     public static ArrayList<String> parseLineOutputValues (String line) {
