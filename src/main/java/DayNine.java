86a861db522dce84f849380478efb5d62eb818af
PensiveDragon <matthew_bfg2k@hotmail.com>
2022-05-15 23:53:33 +0100

    DayNine Part 1: Added parseInput method.

--- src/main/java/DayNine.java
+++ src/main/java/DayNine.java
@@ -1,6 +1,43 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
 public class DayNine {
+
+    static String testInputPath = "src/main/resources/day9_test_input.txt";
+    static String inputPath = "src/main/resources/day9_input.txt";
 
     public static void main(String args[]) {
         System.out.println("Moo");
+
+        parseInput(testInputPath);
     }
+
+    public static ArrayList<String> parseInput(String path) {
+
+        ArrayList<String> allInput = new ArrayList<>();
+
+        try {
+            List<String> allLines = Files.readAllLines(Paths.get(path));
+
+            allInput.addAll(allLines);
+            //System.out.println(allInput);
+            System.out.println("Read in " + allInput.size() + " lines.");
+
+            System.out.println(allInput);
+
+            return allInput;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+}
+
+class caveMap {
+
 }
